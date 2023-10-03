b5dfa7f81587763de917412d3c249ab89e93a0ad
Jan Rychly <jrychlyg@gmail.com>
2023-10-03 12:02:37 -0800

    Implement tab navigation routing - created 3 basic tabs that now change the route

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 // The title is supposed to imitate a code source file header
 import { AppBar, Box, Container, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const a11yProps = (index: number) => ({
   'id': `simple-tab-${index}`,
@@ -8,7 +9,15 @@ const a11yProps = (index: number) => ({
  
 })
 
+const tabs = [
+  { label: 'Full Stack', path: 'full-stack' },
+  { label: 'Front End', path: 'front-end' },
+  { label: 'Back End', path: 'back-end' },
+] as const
+
 export const Navigation = () => {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
   return(
     <AppBar>
       <Container>
@@ -16,8 +25,8 @@ export const Navigation = () => {
           <Typography variant='h4' sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>/* @author: Jan Rychly */</Typography>
           <Typography variant='h6'sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}>J. R.</Typography>
           <Box>
-            <Tabs value={0} >
-              <Tab label="Dev" {...a11yProps(0)}/>
+            <Tabs value={tabs.findIndex((tab) => pathname.includes(tab.path))} >
+              {tabs.map((tab, index) => (<Tab label={tab.label} onClick={() => navigate(tab.path)} key={index} {...a11yProps(index)}/>))}
             </Tabs>
           </Box>
         </Toolbar>
