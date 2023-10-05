1520ff385a6d8231c6894be2fef05a13f303136a
Jan Rychly <jrychlyg@gmail.com>
2023-10-05 15:11:18 -0800

    Add redirection from root to landing page

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,27 @@
 import { Navigation } from './Navigation'
 import { Box, Container } from '@mui/material'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 
-export const Layout = () => (
-  <>
-    <Navigation />
-    {/* a box spacer of the size of the toolbar so that nothing is hidden underneath <Navigation /> */}
-    <Box sx={(theme) => (theme.mixins.toolbar)} />
-    <Container sx={{ px: { xs: 3, md: 20 }, py: 10 }}>
-      <Outlet />
-    </Container>
-  </>
-)
+export const Layout = () => {
+  // redirect to main page from root
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/landing/all', { replace: true })
+    }
+  }, [navigate, pathname])
+
+  return (
+    <>
+      <Navigation />
+      {/* a box spacer of the size of the toolbar so that nothing is hidden underneath <Navigation /> */}
+      <Box sx={(theme) => (theme.mixins.toolbar)} />
+      <Container sx={{ px: { xs: 3, md: 20 }, py: 10 }}>
+        <Outlet />
+      </Container>
+    </>
+  )
+}
