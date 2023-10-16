c1dfd78708a5fe6b735a43e28033d39377c703ca
Jan Rychly <jrychlyg@gmail.com>
2023-10-16 12:11:32 -0800

    Add github links to project details

--- src/pages/Things/Project.tsx
+++ src/pages/Things/Project.tsx
@@ -2,6 +2,7 @@ import { Box, Card, CardProps, Chip, Typography } from '@mui/material'
 import { ListFromMd } from '../../components/ListFromMd'
 import _ from 'lodash'
 import { SpanFromMd } from '../../components/SpanFromMd'
+import { GithubIcon } from '../../icons/GithubIcon'
 
 
 // TODO flag for projects that are displayed on the landing page vs only on the things page 
@@ -10,6 +11,8 @@ type ProjectDetails = {
   summary: string
   description: string[]
   tech: string[]
+  repo?: string
+  link?: string
   date: {
     start?: string | number
     finish?: string | number
@@ -39,6 +42,12 @@ export const Project = ({ details, summary, ...props }: ProjectProps) => (
       ? <Typography><SpanFromMd markdown={details.summary} /></Typography>
       : (<>
         <ListFromMd items={details.description} />
+        {details.repo && (
+          <Typography sx={{ mt: 2 }}>
+            <GithubIcon sx={{ fontSize: '90%', mr: 2 }} />
+            <a href={details.repo}>{details.repo.replace(/http(s?):\/\//, '')}</a>
+          </Typography>
+        )}
         <Box sx={{ mt: 3, display: 'flex', flexWrap: 'wrap', gap: 1 }}>
           {details.tech.map((item, i) => <Chip label={item} variant="outlined" key={i} />)}
         </Box>
