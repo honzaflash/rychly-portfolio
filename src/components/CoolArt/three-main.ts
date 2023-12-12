2c708566159352c02cd575e31f10ea1f5889b8a5
Jan Rychly <jrychlyg@gmail.com>
2023-12-11 16:39:08 -0900

    make the 3D render background transparent

--- src/components/CoolArt/three-main.ts
+++ src/components/CoolArt/three-main.ts
@@ -13,7 +13,7 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000)
 camera.position.z = 10
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ alpha: true })
 renderer.setSize(800, 1000)
 
 const clock = new THREE.Clock()
